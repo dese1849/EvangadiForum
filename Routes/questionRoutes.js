2a1df0c82c5a24e50da0a24992d6f37e6ab2cdba
Desalegn Wendimu <asewen18@gmail.com>
2024-06-25 14:09:37 +0300

    EvanagadiFroum

---
+++ Routes/questionRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+// organize and manage routes in a modular and efficient manner
+const questionRouter = express.Router();
+const {
+  question,
+  selectquestion,
+  selectsinglequestion,
+  selectansawer,
+} = require("../Controller/questionController");
+questionRouter.post("/askquestion", question);
+questionRouter.get("/selectquestion", selectquestion);
+questionRouter.get("/selectsinglequestion", selectsinglequestion);
+questionRouter.get("/selectansawer", selectansawer);
+
+module.exports = questionRouter;
